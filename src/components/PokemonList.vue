b2d24d9ee88260ce8e51891f0d5d0ca4f489fee4
Jimbo Cortes <jimbo.cortes@gmail.com>
2019-09-09 08:55:59 +0800

    Add pokemon list item

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -2,14 +2,19 @@
   <div>
     <h2>Pokemon List</h2>
     <div v-bind:key="pokemon.name" v-for="pokemon in pokemons">
-      <p>{{ pokemon.name }}</p>
+      <PokemonListItem v-bind:pokemon="pokemon" />
     </div>
   </div>
 </template>
 
 <script>
+import PokemonListItem from "./PokemonListItem";
+
 export default {
   name: "PokemonList",
+  components: {
+    PokemonListItem
+  },
   props: ["pokemons"]
 };
 </script>
---
+++ src/components/PokemonListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button>{{ pokemon.name }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonListItem",
+  props: ["pokemon"]
+};
+</script>
+
+<style>
+</style>
